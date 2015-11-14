d9c5bbda5eec5837d15546d1a5b6ed71c0178f16
matthew weintrub <heytrub@gmail.com>
2015-11-13 23:00:21 -0800

    populate data into view controller

--- PullRefresh/ViewController.swift
+++ PullRefresh/ViewController.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    //MARK: properties
 
     @IBOutlet weak var tblDemo: UITableView!
     
+    var dataArray: Array<String> = ["One", "Two", "Three", "Four", "Five"]
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        tblDemo.delegate = self
+        tblDemo.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +28,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    
+    //MARK: table view functions
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray.count
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("idCell", forIndexPath: indexPath) as! UITableViewCell
+        
+        cell.textLabel!.text = dataArray[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 60.0
+    }
 }
 
